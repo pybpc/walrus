9d289701bd9bccdbb18c03f976592efb424d3122
gousaiyang <gousaiyang@163.com>
2020-10-14 23:56:17 -0700

    Enhance f-string test case

--- tests/sample/fstring.py
+++ tests/sample/fstring.py
@@ -7,9 +7,12 @@ class A:
     print(y)
     print(f'''L1 -> {f"""L2 -> {f'L3 -> {f"L4 -> {(z := chr(77))!r} <- L4"} <- L3'} <- L2"""} <- L1''')
     print(z)
+    print(f'see -> {"header " + f"{(w := chr(88))}" + " footer"} <- see')
+    print(w)
     print(locals)
 
 
 print(globals().get('y'))
 print(globals().get('z'))
+print(globals().get('w'))
 print(callable(locals))
